39e77deab5d78828020f8833bf322db778c55995
Leon Chen <lchen3@gmail.com>
2015-09-19 18:50:34 -0400

    update activations test

--- test/test-activations.js
+++ test/test-activations.js
@@ -5,16 +5,15 @@ import { linear, relu, sigmoid, sigmoidHard, tanh, softmax } from '../src/functi
 
 const EPSILON = 0.00001;
 
-describe('Activation functions', function () {
+describe('Activation functions [baseline]', function () {
   let x = [ 0.01, 0.03, -0.01, 0.05, 0 ];
-  let repeat = 18;
+  let repeat = 17;
   while(repeat--) {
     x = x.concat(x);
   }
-  let x_float32 = new Float32Array(x);
 
   describe('linear', function () {
-    it('[baseline] should return expected result', (done) => {
+    it('should return expected result', (done) => {
       let start = new Date().getTime();
       let y = linear(x);
       console.log(`      ${new Date().getTime() - start} ms`);
@@ -25,34 +24,23 @@ describe('Activation functions', function () {
 
   describe('relu', function () {
     let expected = [ 0.01, 0.03, 0, 0.05, 0];
-    let repeat = 18;
+    let repeat = 17;
     while(repeat--) {
       expected = expected.concat(expected);
     }
-    let expected_float32 = new Float32Array(expected);
 
-    it('[baseline] should return expected result', (done) => {
-      let useSIMD = false;
+    it('should return expected result', (done) => {
       let start = new Date().getTime();
-      let y = relu(x, useSIMD);
+      let y = relu(x);
       console.log(`      ${new Date().getTime() - start} ms`);
       assert(y.every((y_i, i) => Math.abs(y_i - expected[i]) < EPSILON));
-      done();
-    });
-
-    it('[SIMD] should return expected result', (done) => {
-      let useSIMD = true;
-      let start = new Date().getTime();
-      let y_float32 = relu(x_float32, useSIMD);
-      console.log(`      ${new Date().getTime() - start} ms`);
-      assert(y_float32.every((y_i, i) => Math.abs(y_i - expected_float32[i]) < EPSILON));
       done();
     });
   });
 
   describe('sigmoid', function () {
     let expected = [ 0.50249998, 0.50749944, 0.49750002, 0.5124974, 0.5 ];
-    let repeat = 18;
+    let repeat = 17;
     while(repeat--) {
       expected = expected.concat(expected);
     }
@@ -68,7 +56,7 @@ describe('Activation functions', function () {
 
   describe('sigmoidHard', function () {
     let expectedSigmoid = [ 0.50249998, 0.50749944, 0.49750002, 0.5124974, 0.5 ];
-    let repeat = 18;
+    let repeat = 17;
     while(repeat--) {
       expectedSigmoid = expectedSigmoid.concat(expectedSigmoid);
     }
@@ -85,7 +73,7 @@ describe('Activation functions', function () {
 
   describe('tanh', function () {
     let expected = [ 0.00999967, 0.029991, -0.00999967, 0.04995837, 0 ];
-    let repeat = 18;
+    let repeat = 17;
     while(repeat--) {
       expected = expected.concat(expected);
     }
@@ -101,7 +89,7 @@ describe('Activation functions', function () {
 
   describe('softmax', function () {
     let expected = [ 0.19875734, 0.20277251, 0.19482169, 0.20686879, 0.19677968 ];
-    let repeat = 18;
+    let repeat = 17;
     while(repeat--) {
       expected = expected.concat(expected);
     }
