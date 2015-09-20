ee36c378ca721c7a9c5c45bf1d00e2be2f71ea3b
Leon Chen <lchen3@gmail.com>
2015-09-19 23:40:11 -0400

    add embedding layer and tests

---
+++ src/layers/embedding.js
@@ -0,0 +1,17 @@
+import ndarray from 'ndarray';
+import ops from 'ndarray-ops';
+
+export function embeddingLayer(x, E) {
+  let nnz = x.data.reduce((a, b) => a + ((b > 0) ? 1 : 0), 0);
+  let y = ndarray(new Float32Array(nnz * E.shape[1]), [nnz, E.shape[1]]);
+
+  let i_nnz = 0;
+  for (let i = 0; i < x.shape[0]; i++) {
+    if (x.get(i) > 0) {
+      ops.assign(y.pick(i_nnz, null), E.pick(x.get(i), null));
+      i_nnz += 1;
+    }
+  }
+
+  return y;
+}
