2729a3f7a973ccc15dde0fb166bb83614ac231f3
Leon Chen <lchen3@gmail.com>
2015-09-20 22:43:05 -0400

    epsilon constant

--- test/test-layers-embedding.js
+++ test/test-layers-embedding.js
@@ -4,6 +4,8 @@ import ndarray from 'ndarray';
 import ops from 'ndarray-ops';
 import { embeddingLayer } from '../src/layers/embedding';
 
+const EPSILON = almostEqual.FLT_EPSILON;
+
 describe('Layer: embedding', function() {
   let input = ndarray(new Int32Array([0,0,0,1,2,3]), [6]);
   let E = ndarray(new Float64Array(4*3), [4, 3]);
@@ -16,7 +18,7 @@ describe('Layer: embedding', function() {
     assert.deepEqual(y.shape, [3, 3]);
     for (let i = 0; i < y.shape[0]; i++) {
       for (let j = 0; j < y.shape[1]; j++) {
-        assert(almostEqual(y.get(i, j), E.get(i+1, j), almostEqual.FLT_EPSILON, almostEqual.FLT_EPSILON));
+        assert(almostEqual(y.get(i, j), E.get(i+1, j), EPSILON, EPSILON));
       }
     }
     done();
