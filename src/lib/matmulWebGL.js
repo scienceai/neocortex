c0d77ef312a1f1ec50a2ea3fa96977ee81d4c52a
Leon Chen <lchen3@gmail.com>
2015-09-22 00:20:35 -0400

    additional webGL code for matmul

--- src/lib/matmulWebGL.js
+++ src/lib/matmulWebGL.js
@@ -11,12 +11,18 @@ export default class MatmulWebGL {
   constructor(opts) {
     opts = opts || {};
 
-    this.matGPU = null;
+    this.matA = null;
+    this.matB = null;
 
-    this.renderBuffer = null;
-    this.frameBuffer = null;
+    this.canvas = null;
+    this.GL = null;
+    this.renderer = null;
+    this.texture = null;
+    this.destTexture = null;
+    this.renderbuffer = null;
+    this.framebuffer = null;
 
-    this.vertexShader = '\n\
+    this.vertexShaderCode = '\n\
 // works on texels passed to texture shader \n\
 #ifdef GL_ES \n\
 	precision highp float; \n\
@@ -31,7 +37,7 @@ void main(void) \n\
 	vTex = aTex; \n\
 }';
 
-    this.fragmentShader = '\n\
+    this.fragmentShaderCode = '\n\
 // Fragment shader performs calculations based on the passed row and column as texture coordinates. \n\
 // Result rendered as 32-bit IEEE754 floating point to the RGBA canvas, readPixel is then used to read out the bytes. \n\
 #ifdef GL_ES \n\
@@ -95,21 +101,155 @@ void main(void) { \n\
 
   }
 
-  init(shape) {
-		let canvas = document.getElementById('matmulWebGL');
-		canvas.height = shape[0];
-		canvas.width = shape[1];
+  init(matA, matB) {
+    this.matA = matA;
+    this.matB = matB;
 
-    if (!this.matGPU) {
+    this.canvas = document.getElementById('matmulWebGL');
+    this.canvas.height = matA.shape[0];
+    this.canvas.width = matB.shape[1];
+
+    if (!this.GL) {
       let ctxAttrs = { premultipliedAlpha: false, preserveDrawingBuffer: false };
-      this.matGPU = canvas.getContext('webgl', ctxAttrs) ||
-                    canvas.getContext('experimental-webgl', ctxAttrs);
+      this.GL = this.canvas.getContext('webgl', ctxAttrs) ||
+                this.canvas.getContext('experimental-webgl', ctxAttrs);
 
-      if (typeof this.matGPU === 'undefined' || this.matGPU === null) throw new Error('webGL not supported.');
+      if (typeof this.GL === 'undefined' || this.GL === null) throw new Error('webGL not supported.');
     }
 
-		this.matGPU.viewport(0, 0, shape[1], shape[0]);
-		return this.matGPU;
+    this.GL.viewport(0, 0, matB.shape[1], matA.shape[0]);
+
+    let vertexShader = this.GL.createShader(this.GL.VERTEX_SHADER);
+    this.GL.shaderSource(vertexShader, this.vertexShaderCode);
+		this.GL.compileShader(vertexShader);
+
+    let fragmentShader = this.GL.createShader(this.GL.FRAGMENT_SHADER);
+    this.GL.shaderSource(fragmentShader, this.fragmentShaderCode);
+		this.GL.compileShader(fragmentShader);
+
+		this.renderer = this.GL.createProgram();
+		this.GL.attachShader(this.renderer, vertexShader);
+		this.GL.attachShader(this.renderer, fragmentShader);
+		this.GL.linkProgram(this.renderer);
+		this.GL.useProgram(this.renderer);
+
+    this._bindTexture();
+    this._bindUniform();
+		this._bindVertices();
+    this._bindFrameBuffer();
+
+  }
+
+  _bindTexture() {
+
+    let rA = this.matA.shape[0]
+      , cA = this.matA.shape[1]
+      , rB = this.matB.shape[0]
+      , cB = this.matB.shape[1];
+
+    let r = Math.max(rA, rB)
+      , c = Math.max(cA, cB);
+
+    let texels = new Float32Array(3 * r * c);
+
+    let count = r * c;
+    if (rA === rB && cA === cB) {
+      let dest = 0
+        , srcA = 0
+        , srcB = 0;
+      do {
+        texels[dest++] = this.matA.data[srcA++];
+        texels[dest++] = this.matB.data[srcB++];
+        dest++;
+      } while(--count);
+    } else {
+      let row = 0
+        , col = 0;
+      do {
+        texels[(row * cA + col) * 3] = this.matA.data[srcA++];
+        texels[(col * rB + row) * 3 + 1] = this.matB[col * rB + row];
+        if (col >= cA) {
+          col = 0;
+          row++;
+        }
+      } while(--count);
+    }
+
+		this.texture = this.GL.createTexture();
+    this.GL.activeTexture(this.GL.TEXTURE0);
+    this.GL.bindTexture(this.GL.TEXTURE_2D, this.texture);
+    this.GL.texImage2D(this.GL.TEXTURE_2D, 0, this.GL.RGB, Math.max(cA, cB), Math.max(rA, rB), 0, this.GL.RGB, this.GL.FLOAT, texels);
+
+    this.GL.texParameteri(this.GL.TEXTURE_2D, this.GL.TEXTURE_WRAP_S, this.GL.CLAMP_TO_EDGE);
+    this.GL.texParameteri(this.GL.TEXTURE_2D, this.GL.TEXTURE_WRAP_T, this.GL.CLAMP_TO_EDGE);
+
+    this.GL.texParameteri(this.GL.TEXTURE_2D, this.GL.TEXTURE_MAG_FILTER, this.GL.NEAREST);
+    this.GL.texParameteri(this.GL.TEXTURE_2D, this.GL.TEXTURE_MIN_FILTER, this.GL.NEAREST);
+		this.GL.uniform1i(this.GL.getUniformLocation(this.renderer,'usampler'), 0);
+
+    this.destTexture = this.GL.createTexture();
+		this.GL.activeTexture(this.GL.TEXTURE2);
+		this.GL.bindTexture(this.GL.TEXTURE_2D, this.destTexture);
+    this.GL.texImage2D(this.GL.TEXTURE_2D, 0, this.GL.RGBA, this.GL.RGBA, this.GL.UNSIGNED_BYTE, this.canvas);
 	}
+
+  _bindUniform() {
+		let length = this.GL.getUniformLocation(this.renderer, 'uLength');
+		let rowOut = this.GL.getUniformLocation(this.renderer, 'uOutRows');
+		let colOut = this.GL.getUniformLocation(this.renderer, 'uOutCols');
+		let stepS = this.GL.getUniformLocation(this.renderer, 'uStepS');
+		let stepT = this.GL.getUniformLocation(this.renderer, 'uStepT');
+
+		this.GL.uniform1i(length, this.matA.shape[1]);
+
+		this.GL.uniform1f(rowOut, this.matA.shape[0]);
+		this.GL.uniform1f(colOut, this.matB.shape[1]);
+		// 3x10 x 10x2 -> 3x2 output, requires 10x10 input texture
+		this.GL.uniform1f(stepS, 1. / Math.max(this.matA.shape[1], this.matB.shape[1]));
+		this.GL.uniform1f(stepT, 1. / Math.max(this.matA.shape[0], this.matB.shape[0]));
+	}
+
+  _bindVertices() {
+		let aPos = this.GL.getAttribLocation(this.renderer, 'aPos');
+		let vertexBuffer = this.GL.createBuffer();
+		this.GL.bindBuffer(this.GL.ARRAY_BUFFER, vertexBuffer);
+		let vertices = [-1.0, -1.0, 0.0, 1.0, -1.0, 0.0, 1.0, 1.0, 0.0, -1.0, 1.0, 0.0];
+		this.GL.bufferData(this.GL.ARRAY_BUFFER, new Float32Array(vertices), this.GL.STATIC_DRAW);
+		this.GL.vertexAttribPointer(aPos, 3, this.GL.FLOAT, false, 0, 0);
+		this.GL.enableVertexAttribArray(aPos);
+
+		let aTex = this.GL.getAttribLocation(this.renderer, 'aTex');
+		let texCoordinates = this.GL.createBuffer();
+		this.GL.bindBuffer(this.GL.ARRAY_BUFFER, texCoordinates);
+		let textureCoords = [0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0];
+		this.GL.bufferData(this.GL.ARRAY_BUFFER, new Float32Array(textureCoords), this.GL.STATIC_DRAW);
+		this.GL.vertexAttribPointer(aTex, 2, this.GL.FLOAT, false, 0, 0);
+		this.GL.enableVertexAttribArray(aTex);
+
+		let indices = this.GL.createBuffer();
+		this.GL.bindBuffer(this.GL.ELEMENT_ARRAY_BUFFER, indices);
+		let vertexIndices = [0, 1, 2, 0, 2, 3];
+		this.GL.bufferData(this.GL.ELEMENT_ARRAY_BUFFER, new Uint16Array(vertexIndices), this.GL.STATIC_DRAW);
+	}
+
+  _bindFrameBuffer() {
+		this.renderbuffer = this.renderbuffer || this.GL.createRenderbuffer();
+		this.GL.bindRenderbuffer(this.GL.RENDERBUFFER, null);
+		this.GL.bindRenderbuffer(this.GL.RENDERBUFFER, this.renderbuffer);
+		this.GL.renderbufferStorage(this.GL.RENDERBUFFER, this.GL.DEPTH_COMPONENT16, this.matB.shape[1], this.matA.shape[0]);
+
+		this.framebuffer = this.framebuffer || this.GL.createFramebuffer();
+		this.GL.bindFramebuffer(this.GL.FRAMEBUFFER, this.framebuffer);
+		this.GL.framebufferTexture2D(this.GL.FRAMEBUFFER, this.GL.COLOR_ATTACHMENT0, this.GL.TEXTURE_2D, this.destTexture, 0);
+		this.GL.framebufferRenderbuffer(this.GL.FRAMEBUFFER, this.GL.DEPTH_ATTACHMENT, this.GL.RENDERBUFFER, this.renderbuffer);
+	}
+
+  multiply() {
+    let buffer = new ArrayBuffer(this.matA.shape[0] * this.matB.shape[1] * 4);
+		let product = new Uint8Array(buffer);
+		this.GL.readPixels(0, 0, this.matB.shape[1], this.matA.shape[0], this.GL.RGBA, this.GL.UNSIGNED_BYTE, product);
+
+    return new Float32Array(buffer);
+  }
 
 }
