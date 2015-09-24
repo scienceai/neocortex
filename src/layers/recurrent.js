9b95e90a945af49056e849d0ef40bbdac6328a9a
Leon Chen <lchen3@gmail.com>
2015-09-24 17:09:01 -0400

    add JZS1-3 layers (corresponds to keras implementation)

--- src/layers/recurrent.js
+++ src/layers/recurrent.js
@@ -147,3 +147,200 @@ export function rGRULayer(x, weights, activation='tanh', innerActivation='sigmoi
 
   return h_t;
 }
+
+
+///////////////////////////////////////////////////////
+// JZS1
+//
+export function rJZS1Layer(x, weights, activation='tanh', innerActivation='sigmoid') {
+  let { W_xz, b_z, W_xr, W_hr, b_r, W_hh, b_h, Pmat } = weights;
+
+  let x_t = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let temp = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let temp2 = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let z_t = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let temp_xz = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let r_t = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let temp_xr = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let temp_hr = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let h_t = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let temp_x_proj = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let temp_hh = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let h_tm1 = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+
+  function _step() {
+    ops.assign(h_tm1, h_t);
+
+    mvprod(temp_xz, W_xz.transpose(1, 0), x_t);
+    ops.assigns(z_t, 0);
+    ops.addeq(z_t, temp_xz);
+    ops.addeq(z_t, b_z);
+    activationFuncs[innerActivation](z_t);
+
+    mvprod(temp_xr, W_xr.transpose(1, 0), x_t);
+    mvprod(temp_hr, W_hr.transpose(1, 0), h_tm1);
+    ops.assigns(r_t, 0);
+    ops.addeq(r_t, temp_xr);
+    ops.addeq(r_t, temp_hr);
+    ops.addeq(r_t, b_r);
+    activationFuncs[innerActivation](r_t);
+
+    mvprod(temp_x_proj, Pmat.transpose(1, 0), x_t);
+    ops.mul(temp, r_t, h_tm1);
+    mvprod(temp_hh, W_hh.transpose(1, 0), temp);
+    ops.assigns(h_t, 0);
+    ops.addeq(h_t, activationFuncs['tanh'](temp_x_proj));
+    ops.addeq(h_t, temp_hh);
+    ops.addeq(h_t, b_h);
+    activationFuncs[activation](h_t);
+
+    ops.mul(temp, z_t, h_t);
+    ops.assign(temp2, z_t);
+    ops.addseq(ops.negeq(temp2), 1);
+    ops.muleq(temp2, h_tm1);
+    ops.addeq(temp2, temp);
+    ops.assign(h_t, temp2);
+  }
+
+  ops.assigns(h_t, 0);
+  for (let i = 0; i < x.shape[0]; i++) {
+    ops.assign(x_t, x.pick(i, null));
+    _step();
+  }
+
+  return h_t;
+}
+
+
+///////////////////////////////////////////////////////
+// JZS2
+//
+export function rJZS2Layer(x, weights, activation='tanh', innerActivation='sigmoid') {
+  let { W_xz, W_hz, b_z, W_hr, b_r, W_xh, W_hh, b_h, Pmat } = weights;
+
+  let x_t = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let temp_x_proj = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let temp = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let temp2 = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let z_t = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let temp_xz = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let temp_hz = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let r_t = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let temp_hr = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let h_t = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let temp_xh = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let temp_hh = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let h_tm1 = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+
+  function _step() {
+    ops.assign(h_tm1, h_t);
+
+    mvprod(temp_xz, W_xz.transpose(1, 0), x_t);
+    mvprod(temp_hz, W_hz.transpose(1, 0), h_tm1);
+    ops.assigns(z_t, 0);
+    ops.addeq(z_t, temp_xz);
+    ops.addeq(z_t, temp_hz);
+    ops.addeq(z_t, b_z);
+    activationFuncs[innerActivation](z_t);
+
+    mvprod(temp_x_proj, Pmat.transpose(1, 0), x_t);
+    mvprod(temp_hr, W_hr.transpose(1, 0), h_tm1);
+    ops.assigns(r_t, 0);
+    ops.addeq(r_t, temp_x_proj);
+    ops.addeq(r_t, temp_hr);
+    ops.addeq(r_t, b_r);
+    activationFuncs[innerActivation](r_t);
+
+    mvprod(temp_xh, W_xh.transpose(1, 0), x_t);
+    ops.mul(temp, r_t, h_tm1);
+    mvprod(temp_hh, W_hh.transpose(1, 0), temp);
+    ops.assigns(h_t, 0);
+    ops.addeq(h_t, temp_xh);
+    ops.addeq(h_t, temp_hh);
+    ops.addeq(h_t, b_h);
+    activationFuncs[activation](h_t);
+
+    ops.mul(temp, z_t, h_t);
+    ops.assign(temp2, z_t);
+    ops.addseq(ops.negeq(temp2), 1);
+    ops.muleq(temp2, h_tm1);
+    ops.addeq(temp2, temp);
+    ops.assign(h_t, temp2);
+  }
+
+  ops.assigns(h_t, 0);
+  for (let i = 0; i < x.shape[0]; i++) {
+    ops.assign(x_t, x.pick(i, null));
+    _step();
+  }
+
+  return h_t;
+}
+
+
+///////////////////////////////////////////////////////
+// JZS3
+//
+export function rJZS3Layer(x, weights, activation='tanh', innerActivation='sigmoid') {
+  let { W_xz, W_hz, b_z, W_xr, W_hr, b_r, W_xh, W_hh, b_h } = weights;
+
+  let x_t = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let temp = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let temp2 = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let z_t = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let temp_xz = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let temp_hz = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let r_t = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let temp_xr = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let temp_hr = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let h_t = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let temp_xh = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let temp_hh = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let h_tm1 = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+  let h_tm1_temp = ndarray(new Float64Array(x.shape[1]), [x.shape[1]]);
+
+  function _step() {
+    ops.assign(h_tm1, h_t);
+
+    mvprod(temp_xz, W_xz.transpose(1, 0), x_t);
+    ops.assign(h_tm1_temp, h_tm1);
+    mvprod(temp_hz, W_hz.transpose(1, 0), activationFuncs['tanh'](h_tm1_temp));
+    ops.assigns(z_t, 0);
+    ops.addeq(z_t, temp_xz);
+    ops.addeq(z_t, temp_hz);
+    ops.addeq(z_t, b_z);
+    activationFuncs[innerActivation](z_t);
+
+    mvprod(temp_xr, W_xr.transpose(1, 0), x_t);
+    mvprod(temp_hr, W_hr.transpose(1, 0), h_tm1);
+    ops.assigns(r_t, 0);
+    ops.addeq(r_t, temp_xr);
+    ops.addeq(r_t, temp_hr);
+    ops.addeq(r_t, b_r);
+    activationFuncs[innerActivation](r_t);
+
+    mvprod(temp_xh, W_xh.transpose(1, 0), x_t);
+    ops.mul(temp, r_t, h_tm1);
+    mvprod(temp_hh, W_hh.transpose(1, 0), temp);
+    ops.assigns(h_t, 0);
+    ops.addeq(h_t, temp_xh);
+    ops.addeq(h_t, temp_hh);
+    ops.addeq(h_t, b_h);
+    activationFuncs[activation](h_t);
+
+    ops.mul(temp, z_t, h_t);
+    ops.assign(temp2, z_t);
+    ops.addseq(ops.negeq(temp2), 1);
+    ops.muleq(temp2, h_tm1);
+    ops.addeq(temp2, temp);
+    ops.assign(h_t, temp2);
+  }
+
+  ops.assigns(h_t, 0);
+  for (let i = 0; i < x.shape[0]; i++) {
+    ops.assign(x_t, x.pick(i, null));
+    _step();
+  }
+
+  return h_t;
+}
--- test/test-layers-recurrent.js
+++ test/test-layers-recurrent.js
@@ -2,7 +2,7 @@ import assert from 'assert';
 import almostEqual from 'almost-equal';
 import ndarray from 'ndarray';
 import pack from 'ndarray-pack';
-import { rLSTMLayer, rGRULayer } from '../src/layers/recurrent';
+import { rLSTMLayer, rGRULayer, rJZS1Layer, rJZS2Layer, rJZS3Layer } from '../src/layers/recurrent';
 
 const EPSILON = almostEqual.FLT_EPSILON;
 
@@ -40,6 +40,66 @@ describe('Layer: recurrent', function() {
 
       let y = rGRULayer(input, weights);
       let expected = new Float64Array([0.5854064873930955, 0.6566667408032925, 0.3494883836663248, 0.3578150449007096]);
+
+      assert.deepEqual(y.shape, [4]);
+      for (let i = 0; i < y.shape[0]; i++) {
+        assert(almostEqual(y.get(i), expected[i], EPSILON, EPSILON));
+      }
+      done();
+    });
+  });
+
+  describe('mutated recurrent network 1 (JZS1) serialized from Keras', function() {
+    it('should output the correct hidden state at the last timestep', (done) => {
+      let weights = require('./fixtures/test_weights_JZS1_keras.json');
+      for (let key in weights) {
+        // pack creates Float64Array ndarrays
+        // TODO: need to convert to Float32Array if set as default
+        weights[key] = pack(weights[key]);
+      }
+
+      let y = rJZS1Layer(input, weights);
+      let expected = new Float64Array([0.7362973093986511, -0.023125506937503815, 0.8724695444107056, 0.316345751285553]);
+
+      assert.deepEqual(y.shape, [4]);
+      for (let i = 0; i < y.shape[0]; i++) {
+        assert(almostEqual(y.get(i), expected[i], EPSILON, EPSILON));
+      }
+      done();
+    });
+  });
+
+  describe('mutated recurrent network 2 (JZS2) serialized from Keras', function() {
+    it('should output the correct hidden state at the last timestep', (done) => {
+      let weights = require('./fixtures/test_weights_JZS2_keras.json');
+      for (let key in weights) {
+        // pack creates Float64Array ndarrays
+        // TODO: need to convert to Float32Array if set as default
+        weights[key] = pack(weights[key]);
+      }
+
+      let y = rJZS2Layer(input, weights);
+      let expected = new Float64Array([0.4409944713115692, 0.6610596179962158, 0.22975823283195496, -0.12029259651899338]);
+
+      assert.deepEqual(y.shape, [4]);
+      for (let i = 0; i < y.shape[0]; i++) {
+        assert(almostEqual(y.get(i), expected[i], EPSILON, EPSILON));
+      }
+      done();
+    });
+  });
+
+  describe('mutated recurrent network 3 (JZS3) serialized from Keras', function() {
+    it('should output the correct hidden state at the last timestep', (done) => {
+      let weights = require('./fixtures/test_weights_JZS3_keras.json');
+      for (let key in weights) {
+        // pack creates Float64Array ndarrays
+        // TODO: need to convert to Float32Array if set as default
+        weights[key] = pack(weights[key]);
+      }
+
+      let y = rJZS3Layer(input, weights);
+      let expected = new Float64Array([0.8178967237472534, 0.1090848445892334, 0.5106683969497681, 0.5130321979522705]);
 
       assert.deepEqual(y.shape, [4]);
       for (let i = 0; i < y.shape[0]; i++) {
