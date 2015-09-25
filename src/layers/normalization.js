64fcefb7e8b23e52d092ef86b67dba62bfe5a36d
Leon Chen <lchen3@gmail.com>
2015-09-25 18:44:35 -0400

    in batch normalization, set epsilon as param

--- src/layers/normalization.js
+++ src/layers/normalization.js
@@ -3,10 +3,10 @@ import ops from 'ndarray-ops';
 
 const EPSILON = 0.000001;
 
-export function batchNormalization(x, weights) {
+export function batchNormalization(x, weights, epsilon=EPSILON) {
   let { gamma, beta, mean, std } = weights;
 
-  ops.addeq(ops.muleq(ops.diveq(ops.subeq(x, mean), ops.addseq(std, EPSILON)), gamma), beta);
+  ops.addeq(ops.muleq(ops.diveq(ops.subeq(x, mean), ops.addseq(std, epsilon)), gamma), beta);
 
   return x;
 }
