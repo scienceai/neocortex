755b5431984c4cc648e4f0176c0ebb9329b109f0
Leon Chen <lchen3@gmail.com>
2015-09-25 19:28:13 -0400

    update dropout layer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import * as activationFuncs from './functions/activations';
 import { denseLayer } from './layers/dense';
 import { embeddingLayer } from './layers/embedding';
 import { batchNormalizationLayer } from './layers/normalization';
+import { dropoutLayer } from './layers/dropout';
 import { rLSTMLayer, rGRULayer, rJZS1Layer, rJZS2Layer, rJZS3Layer } from './layers/recurrent';
 
 import MatmulWebGL from './lib/matmulWebGL';
@@ -13,6 +14,7 @@ export {
   denseLayer,
   embeddingLayer,
   batchNormalizationLayer,
+  dropoutLayer,
   rLSTMLayer,
   rGRULayer,
   rJZS1Layer,
