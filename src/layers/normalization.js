667daef37a9171f102ce61b7e58d335e135ea191
Leon Chen <lchen3@gmail.com>
2015-09-29 07:49:14 -0400

    fix batch normalization layer name

--- src/layers/normalization.js
+++ src/layers/normalization.js
@@ -3,7 +3,7 @@ import ops from 'ndarray-ops';
 
 const EPSILON = 0.000001;
 
-export function batchNormalization(x, weights, epsilon=EPSILON) {
+export function batchNormalizationLayer(x, weights, epsilon=EPSILON) {
   let { gamma, beta, mean, std } = weights;
 
   ops.addeq(ops.muleq(ops.diveq(ops.subeq(x, mean), ops.addseq(std, epsilon)), gamma), beta);
