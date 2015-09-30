aabad068903513d9d6356c0d8cafef1fbb9f77f5
Leon Chen <lchen3@gmail.com>
2015-09-29 20:28:39 -0400

    add test for dropoutLayer

---
+++ test/test-layers-dropout.js
@@ -0,0 +1,28 @@
+import assert from 'assert';
+import almostEqual from 'almost-equal';
+import ndarray from 'ndarray';
+import { dropoutLayer } from '../src/layers/dropout';
+
+describe('Layer: dropout', function() {
+  let x = ndarray(new Float64Array([0.5, 0.6, 1.2]), [3]);
+
+  it('should produce expected', (done) => {
+    let expected = [0.25, 0.3, 0.6];
+    let y = dropoutLayer(Float64Array, x, 0.5);
+    assert.deepEqual(y.shape, [3]);
+    for (let i = 0; i < y.shape[0]; i++) {
+      assert(almostEqual(y.get(i), expected[i], almostEqual.FLT_EPSILON, almostEqual.FLT_EPSILON));
+    }
+    done();
+  });
+
+  it('should produce expected', (done) => {
+    let expected = [0.05, 0.06, 0.12];
+    let y = dropoutLayer(Float64Array, x, 0.1);
+    assert.deepEqual(y.shape, [3]);
+    for (let i = 0; i < y.shape[0]; i++) {
+      assert(almostEqual(y.get(i), expected[i], almostEqual.FLT_EPSILON, almostEqual.FLT_EPSILON));
+    }
+    done();
+  });
+});
