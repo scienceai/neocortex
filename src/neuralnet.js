590fe01ccd1fdbcaa6a548028998ca51bb44cf98
Leon Chen <lchen3@gmail.com>
2015-10-06 19:39:47 -0400

    promisify neuralnet methods with bluebird

--- src/neuralnet.js
+++ src/neuralnet.js
@@ -1,133 +1,134 @@
 import * as layerFuncs from './layers';
 import ndarray from 'ndarray';
 import pack from 'ndarray-pack';
+import unpack from 'ndarray-unpack';
 import request from 'superagent';
 import zlib from 'zlib';
 import concat from 'concat-stream';
+import Promise from 'bluebird';
 
 export default class NeuralNet {
   constructor(config) {
     config = config || {};
 
-    this.arrayType = Float64Array || Array;
+    this._arrayType = Float64Array || Array;
     if (config.arrayType === 'float32') {
-      this.arrayType = Float32Array;
+      this._arrayType = Float32Array;
     } else if (config.arrayType === 'float64') {
-      this.arrayType = Float64Array;
+      this._arrayType = Float64Array;
     }
 
-    this._SIMD_AVAIL = (this.arrayType === Float32Array || this.arrayType === Float64Array) && ('SIMD' in this);
+    this._SIMD_AVAIL = (this._arrayType === Float32Array || this._arrayType === Float64Array) && ('SIMD' in this);
     this._WEBGL_AVAIL = true;
 
     this.useGPU = (config.useGPU || false) && this._WEBGL_AVAIL;
 
-    if (typeof window !== 'undefined') {
+    if (typeof window === 'object') {
       this._environment = 'browser';
-    } else {
+    } else if (typeof importScripts === 'function') {
+      this._environment = 'webworker';
+    } else if (typeof process === 'object' && typeof require === 'function') {
       this._environment = 'node';
+    } else {
+      this._environment = 'shell';
     }
+    console.log(`Running from environment: ${this._environment}`);
 
-    this.readyStatus = false;
     this._layers = [];
 
-    if (config.modelFilePath) {
-      this.loadModel(config.modelFilePath);
-    } else {
-      throw new Error('no modelFilePath specified in config object.');
-    }
-
-    this.sampleDataLoaded = false;
-
-    if (config.sampleDataPath) {
-      this.loadSampleData(config.sampleDataPath);
-    }
+    this._modelFilePath = config.modelFilePath || null;
+    this._sampleDataPath = config.sampleDataPath || null;
   }
 
-  loadModel(modelFilePath) {
-    if (this._environment === 'node') {
-      let s = fs.createReadStream(__dirname + modelFilePath);
-      if (modelFilePath.endsWith('.json.gz')) {
-        let gunzip = zlib.createGunzip();
-        s.pipe(gunzip).pipe(concat((model) => {
-          this._layers = JSON.parse(model.toString());
-          this.readyStatus = true;
-        }));
-      } else if (modelFilePath.endsWith('.json')) {
-        s.pipe(concat((model) => {
-          this._layers = JSON.parse(model.toString());
-          this.readyStatus = true;
-        }));
+  loadModel() {
+    return new Promise((resolve, reject) => {
+      if (!this._modelFilePath) {
+        reject(new Error('no modelFilePath specified in config object.'));
       }
-    } else if (this._environment === 'browser') {
-      request.get(modelFilePath)
-        .end((err, res) => {
-          if (err) return console.error('error loading model file.');
-          if (res.statusCode == 200) {
-            this._layers = res.body;
-            this.readyStatus = true;
-          } else {
-            console.error('error loading model file.');
-          }
-        });
-    }
-  }
 
-  loadSampleData(sampleDataPath) {
-    if (this._environment === 'node') {
-      let s = fs.createReadStream(__dirname + sampleDataPath);
-      if (sampleDataPath.endsWith('.json.gz')) {
-        let gunzip = zlib.createGunzip();
-        s.pipe(gunzip).pipe(concat((data) => {
-          this.SAMPLE_DATA = JSON.parse(data.toString());
-          this.sampleDataLoaded = true;
-        }));
-      } else if (sampleDataPath.endsWith('.json')) {
-        s.pipe(concat((data) => {
-          this.SAMPLE_DATA = JSON.parse(data.toString());
-          this.sampleDataLoaded = true;
-        }));
-      }
-    } else if (this._environment === 'browser') {
-      request.get(sampleDataPath)
-        .end((err, res) => {
-          if (err) return console.error('error loading data file.');
-          if (res.statusCode == 200) {
-            this.SAMPLE_DATA = res.body;
-            this.sampleDataLoaded = true;
-          } else {
-            console.error('error loading data file.');
-          }
-        });
-    }
-  }
-
-  predict(input, callback) {
-
-    let _predict = (X) => {
-      for (let layer of this._layers) {
-        let start = new Date().getTime();
-        let { layerName, parameters } = layer;
-        X = layerFuncs[layerName](this.arrayType, X, ...parameters);
-        console.log(layerName, new Date().getTime() - start, X);
-      }
-      return X;
-    };
-
-    let X = pack(input);
-
-    if (!this.readyStatus) {
-      let waitReady = setInterval(() => {
-        if (this.readyStatus) {
-          clearInterval(waitReady);
-          let output = _predict(X);
-          callback(null, output);
+      if (this._environment === 'browser' || this._environment === 'webworker') {
+        request.get(this._modelFilePath)
+          .end((err, res) => {
+            if (err) reject(err);
+            if (res.statusCode == 200) {
+              this._layers = res.body;
+              resolve(true);
+            } else {
+              reject(new Error('error loading model file.'));
+            }
+          });
+      } else if (this._environment === 'node') {
+        let s = fs.createReadStream(__dirname + this._modelFilePath);
+        if (this._modelFilePath.endsWith('.json.gz')) {
+          let gunzip = zlib.createGunzip();
+          s.pipe(gunzip).pipe(concat((model) => {
+            this._layers = JSON.parse(model.toString());
+            resolve(true);
+          }));
+        } else if (this._modelFilePath.endsWith('.json')) {
+          s.pipe(concat((model) => {
+            this._layers = JSON.parse(model.toString());
+            resolve(true);
+          }));
         }
-      }, 10);
-    } else {
-      let output = _predict(X);
-      callback(null, output);
-    }
+      }
+    });
+  }
 
+  loadSampleData() {
+    return new Promise((resolve, reject) => {
+      if (!this._sampleDataPath) {
+        reject(new Error('no sampleDataPath specified in config object.'));
+      }
+
+      if (this._environment === 'node') {
+        let s = fs.createReadStream(__dirname + this._sampleDataPath);
+        if (this._sampleDataPath.endsWith('.json.gz')) {
+          let gunzip = zlib.createGunzip();
+          s.pipe(gunzip).pipe(concat((data) => {
+            this.SAMPLE_DATA = JSON.parse(data.toString());
+            resolve(true);
+          }));
+        } else if (this._sampleDataPath.endsWith('.json')) {
+          s.pipe(concat((data) => {
+            this.SAMPLE_DATA = JSON.parse(data.toString());
+            resolve(true);
+          }));
+        }
+      } else if (this._environment === 'browser' || this._environment === 'webworker') {
+        request.get(this._sampleDataPath)
+          .end((err, res) => {
+            if (err) reject(err);
+            if (res.statusCode == 200) {
+              this.SAMPLE_DATA = res.body;
+              resolve(true);
+            } else {
+              reject(new Error('error loading data file.'));
+            }
+          });
+      }
+    });
+  }
+
+  predict(input) {
+    return new Promise((resolve, reject) => {
+
+      let _predict = (X) => {
+        for (let layer of this._layers) {
+          let start = new Date().getTime();
+          let { layerName, parameters } = layer;
+          X = layerFuncs[layerName](this._arrayType, X, ...parameters);
+          console.log(layerName, new Date().getTime() - start, X);
+        }
+        return X;
+      };
+
+      let X = pack(input);
+      let output_ndarray = _predict(X);
+
+      resolve(unpack(output_ndarray));
+
+    });
   }
 
 }
