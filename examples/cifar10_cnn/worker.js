9b29f7a29fb52ac959471b0e1e5aa8bd7b78afc2
Leon Chen <lchen3@gmail.com>
2015-10-09 19:13:05 -0400

    update cifar-10 example

--- examples/cifar10_cnn/worker.js
+++ examples/cifar10_cnn/worker.js
@@ -1,11 +1,9 @@
 'use strict';
 
 importScripts('/neuralnet-predict.min.js');
-importScripts('/bluebird.min.js');
 
 let nn = new NeuralNet({
   modelFilePath: '/cifar10_cnn/cifar10_cnn_model_params.json',
-  sampleDataPath: '/cifar10_cnn/sample_data.json',
   arrayType: 'float32',
   useGPU: false
 });
@@ -16,8 +14,7 @@ self.onmessage = function(e) {
   onLoadMsgs.push(e);
 };
 
-Promise.all([nn.loadModel(), nn.loadSampleData()])
-  .then(function() {
+nn.loadModel().then(function() {
 
     function handleMsg(e) {
 
@@ -39,7 +36,6 @@ Promise.all([nn.loadModel(), nn.loadSampleData()])
     // handle additional messages
     self.onmessage = handleMsg;
 
-  })
-  .catch(function(e) {
-    console.error(e);
-  });
+}).catch(function(e) {
+  console.error(e);
+});
