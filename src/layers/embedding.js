0bc93d2401491ca0f4e579259e72ef70ea2e0451
Leon Chen <lchen3@gmail.com>
2015-10-20 03:22:34 -0400

    add zero-masking option in embedding layer, with test

--- src/layers/embedding.js
+++ src/layers/embedding.js
@@ -2,18 +2,31 @@ import ndarray from 'ndarray';
 import ops from 'ndarray-ops';
 import pack from 'ndarray-pack';
 
-export function embeddingLayer(arrayType, x, weights) {
+export function embeddingLayer(arrayType, x, weights, mask_zero=false) {
   let E = pack(weights['E']);
 
-  let nnz = x.data.reduce((a, b) => a + ((b > 0) ? 1 : 0), 0);
-  let y = ndarray(new arrayType(nnz * E.shape[1]), [nnz, E.shape[1]]);
+  let y;
 
-  let i_nnz = 0;
-  for (let i = 0; i < x.shape[0]; i++) {
-    if (x.get(i) > 0) {
-      ops.assign(y.pick(i_nnz, null), E.pick(x.get(i), null));
-      i_nnz += 1;
+  if (mask_zero) {
+    let nnz = x.data.reduce((a, b) => a + ((b > 0) ? 1 : 0), 0);
+
+    y = ndarray(new arrayType(nnz * E.shape[1]), [nnz, E.shape[1]]);
+
+    let i_nnz = 0;
+    for (let i = 0; i < x.shape[0]; i++) {
+      if (x.get(i) > 0) {
+        ops.assign(y.pick(i_nnz, null), E.pick(x.get(i), null));
+        i_nnz += 1;
+      }
     }
+  } else {
+
+    y = ndarray(new arrayType(x.shape[0] * E.shape[1]), [x.shape[0], E.shape[1]]);
+
+    for (let i = 0; i < x.shape[0]; i++) {
+      ops.assign(y.pick(i, null), E.pick(x.get(i), null));
+    }
+
   }
 
   return y;
--- test/test-layers-embedding.js
+++ test/test-layers-embedding.js
@@ -8,18 +8,25 @@ const EPSILON = almostEqual.FLT_EPSILON;
 
 describe('Layer: embedding', function() {
   let input = ndarray(new Int32Array([0,0,0,1,2,3]), [6]);
-  /*let E = ndarray(new Float64Array(4*3), [4, 3]);
-  ops.assign(E.pick(1, null), ndarray(new Float64Array([0.1, 0.2, 0.3]), [3]));
-  ops.assign(E.pick(2, null), ndarray(new Float64Array([0.4, 0.5, 0.6]), [3]));
-  ops.assign(E.pick(3, null), ndarray(new Float64Array([0.7, 0.8, 0.9]), [3]));*/
   let E = [[0, 0, 0], [0.1, 0.2, 0.3], [0.4, 0.5, 0.6], [0.7, 0.8, 0.9]];
 
   it('should create zero-masked embedding matrix', (done) => {
-    let y = embeddingLayer(Float64Array, input, { E });
+    let y = embeddingLayer(Float64Array, input, { E }, true);
     assert.deepEqual(y.shape, [3, 3]);
     for (let i = 0; i < y.shape[0]; i++) {
       for (let j = 0; j < y.shape[1]; j++) {
         assert(almostEqual(y.get(i, j), E[i+1][j], EPSILON, EPSILON));
+      }
+    }
+    done();
+  });
+
+  it('should create non-zero-masked embedding matrix', (done) => {
+    let y = embeddingLayer(Float64Array, input, { E }, false);
+    assert.deepEqual(y.shape, [6, 3]);
+    for (let i = 0; i < y.shape[0]; i++) {
+      for (let j = 0; j < y.shape[1]; j++) {
+        assert(almostEqual(y.get(i, j), E[input.get(i)][j], EPSILON, EPSILON));
       }
     }
     done();
