18a90ecb223bf42cfc2df41cd296f000a9791a6f
Leon Chen <lchen3@gmail.com>
2015-11-10 21:09:14 -0500

    fix examples server to run in older versions of node #6

--- examples/server.js
+++ examples/server.js
@@ -1,21 +1,7 @@
-'use strict';
+var express = require('express');
+var app = express();
 
-let express = require('express');
-let app = express();
-
-// set proper headers for json and gzipped-json files
-app.use(express.static(__dirname, {
-  setHeaders: function(res, path, stat) {
-    if (path.endsWith('.json.gz')) {
-      res.set({
-        'Content-Encoding': 'gzip',
-        'Content-Type': 'application/json'
-      });
-    } else if (path.endsWith('.json')) {
-      res.set('Content-Type', 'application/json');
-    }
-  }
-}));
+app.use(express.static(__dirname));
 
 app.listen(8000, function() {
   console.log('Examples server running at http://localhost:8000');
