c5d74617256b4b4b98030634dff9eea21b1a599a
Leon Chen <lchen3@gmail.com>
2015-11-17 17:15:44 -0500

    add rescale argument to dropout

--- src/layers/dropout.js
+++ src/layers/dropout.js
@@ -1,10 +1,17 @@
 import ndarray from 'ndarray';
 import ops from 'ndarray-ops';
 
-export function dropoutLayer(arrayType, x, p=0.5) {
+export function dropoutLayer(arrayType, x, p=0.5, rescale=false) {
 
-  let y = ndarray(new arrayType(x.size), x.shape);
-  ops.muls(y, x, 1 - p);
+  // If rescale, rescaling is done at test time,
+  // whereas if false, it is assumed to have been done at training time.
+  // In Keras, currently this is done during training time and rescale should be false
+  if (rescale) {
+    let y = ndarray(new arrayType(x.size), x.shape);
+    ops.muls(y, x, 1 - p);
+    return y;
+  } else {
+    return x;
+  }
 
-  return y;
 }
